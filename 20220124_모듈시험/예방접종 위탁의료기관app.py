10378ab73267358c810e41517eb1dce0bc0242ab
Yoo-sangsu <87604747+highsue@users.noreply.github.com>
2022-01-24 17:50:45 +0900

    Update 예방접종 위탁의료기관app.py

--- 20220124_모듈시험/예방접종 위탁의료기관app.py
+++ 20220124_모듈시험/예방접종 위탁의료기관app.py
@@ -12,7 +12,7 @@ def FlaskData():
     keyValue =r"9XYEAg2P7uoRGqGJIn%2FPZ4KMMSirySVFYQ6CDZUisRAuaDnoHlTsf1PGv%2F74TxRhQtTZdY1F5dTghp0OgKDwWw%3D%3D"  
     dataURL = "https://api.odcloud.kr/api/apnmOrg/v1/list?"
     dataURL += "page=" + str(1) + "&perPage=" + str(10)  
-    dataURL += "&cond" + "%5BorgZipaddr%3A%3ALIKE%5D=%EC%A2%85%EB%A1%9C%EA%B5%AC"
+    dataURL += "&cond" + "%5BorgZipaddr%3A%3ALIKE%5D=%EC%A2%85%EB%A1%9C%EA%B5%AC"+"=종로구"
     dataURL += "&serviceKey=" + keyValue
     dataResult = requests.get(dataURL)
     return json.loads(dataResult)  
